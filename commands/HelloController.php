a814e401c48a4864110a24e6b736e241f5c8b8e2
xinfeiyou <admin@xinfeiyou.com>
2018-01-31 17:42:29 +0800

    修改获取token值

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
@@ -8,7 +9,7 @@
 namespace app\commands;
 
 use yii\console\Controller;
-
+use app\modules\base\models\WorkConfig;
 /**
  * This command echoes the first argument that you have entered.
  *
@@ -17,14 +18,34 @@ use yii\console\Controller;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class HelloController extends Controller
-{
+class HelloController extends Controller {
+
     /**
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
      */
-    public function actionIndex($message = 'hello world')
-    {
+    public function actionIndex($message = 'hello world') {
         echo $message . "\n";
     }
+
+    public function actionToken() {
+        echo $this->getToken();
+    }
+
+    /**
+     * 更新token值
+     * @return type
+     */
+    public function getToken() {
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . \Yii::$app->params['appId'] . "&secret=" . \Yii::$app->params['appSecret'];
+        //$json = '{"access_token":"6_BcxREiAaFsFf8A6MJmW1YfeRUK13ku_E0d9RltpuNhZsG0pCzqtH1SFe1iOYHowpKCwgTUZ4v_u4PJmgc9WjBNdJeFmXW1ouRSv9-NYPhJYOMbf1yp5Dh1Sr1KLOBpUWX0F83MzqWAyPOBd5THThAGAHVH","expires_in":7200}';
+        $json = file_get_contents($url);
+        $arData = json_decode($json, true);
+        if(!empty($arData['access_token'])){
+            return (new WorkConfig())->editKeyToValue('strToken', $arData['access_token']);
+        }else{
+            return $json;
+        }
+    }
+
 }
--- modules/api/controllers/BaseController.php
+++ modules/api/controllers/BaseController.php
@@ -28,18 +28,6 @@ class BaseController extends Controller {
         $arData = json_decode($json, true);
         return $arData['openid'];
     }
-
-    /**
-     * 更新token值
-     * @return type
-     */
-    public function getToken() {
-        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . \Yii::$app->params['appId'] . "&secret=" . \Yii::$app->params['appSecret'];
-        //$json = '{"access_token":"6_BcxREiAaFsFf8A6MJmW1YfeRUK13ku_E0d9RltpuNhZsG0pCzqtH1SFe1iOYHowpKCwgTUZ4v_u4PJmgc9WjBNdJeFmXW1ouRSv9-NYPhJYOMbf1yp5Dh1Sr1KLOBpUWX0F83MzqWAyPOBd5THThAGAHVH","expires_in":7200}';
-        $json = file_get_contents($url);
-        $arData = json_decode($json, true);
-        return (new WorkConfig())->editKeyToValue('strToken', $arData['access_token']);
-    }
     /**
      * 根据经纬度获取城市信息
      * @param string $latitude  经度
